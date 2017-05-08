591a1d1a21dd60c4acba0414780c7d3b6e57a4b3
Brett Berry <brett.elizabeth.berry@gmail.com>
2017-05-08 12:56:13 -0700

    added react scroll

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import FaAngleDoubleDown from 'react-icons/lib/fa/angle-double-down';
 import Waypoint from 'react-waypoint';
 import classnames from 'classnames';
 import Instafeed from 'instafeed.js';
+import { Link as ScrollLink } from 'react-scroll';
 
 import Menu from '../components/Menu';
 import LogoSVG from '../components/LogoSVG';
@@ -28,16 +29,18 @@ class Home extends Component {
     return (
       <div className="intro">
         <LogoSVG className="loadingAnimation"/>
-        <FaAngleDoubleDown className="more"/>
+        <ScrollLink to="nextSection" smooth={true} duration={800}>
+          <FaAngleDoubleDown className="more"/>
+        </ScrollLink>
         <Waypoint onLeave={() => this.setState({ showNavBar: true })}/>
         <div className={classnames('navBarContainer', this.state.showNavBar && 'showNavBar')}>
           <Menu className="menu" onClick={() => this.setState({ showMenu: !this.state.showMenu })}/>
         </div>
-        <div className="section">
-          <h1 className="welcome">welcome</h1>
+        <div className="section" id="nextSection">
+          {/* <h1 className="welcome">welcome</h1> */}
         </div>
-        <div className="testSection" id="instafeed"/>
         <div className="footer"/>
+        <div className="testSection" id="instafeed"/>
       </div>
     );
   }
